c026a475dfdcb29d6606ff42431361ece4b185ce
AbhilashMadi <madiabhilash1563@gmail.com>
2024-05-04 06:28:56 +0530

    correct sider sizes

--- src/pages/workflow.tsx
+++ src/pages/workflow.tsx
@@ -10,7 +10,7 @@ const WorkFlow: FC = () => {
       </div>
     </ResizablePanel>
     <ResizableHandle withHandle />
-    <ResizablePanel defaultSize={25} minSize={25}>
+    <ResizablePanel defaultSize={25} minSize={25} maxSize={92}>
       <ResizablePanelGroup direction="horizontal">
         <ResizablePanel defaultSize={75}>
           <div className="flex h-full items-center justify-center p-6">
@@ -18,7 +18,7 @@ const WorkFlow: FC = () => {
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={25} maxSize={25}>
+        <ResizablePanel defaultSize={25} maxSize={25} minSize={15}>
           <div className="flex h-full items-center justify-center p-6">
             <span className="font-semibold">Min Sider</span>
           </div>
